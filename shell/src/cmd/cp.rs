84f3fa068b8116d61754e11f465f352bac3ae3cc
mndoumghar <mohamedndoumghar@gmail.com>
2026-03-30 16:54:41 +0100

    Add cp: <cmd> Command Implementation s To 0-Shell

--- shell/src/cmd/cp.rs
+++ shell/src/cmd/cp.rs
@@ -1,85 +1,61 @@
 use crate::error::ShellError;
-pub use std::fs;
-pub use std::path::Path;
+use std::collections::HashSet;
+use std::fs;
+use std::path::Path;
+
 #[derive(Debug, Clone)]
-
-
 pub struct Cp {
-    pub flag: bool,
     pub targets: Vec<String>,
 }
 
 impl Cp {
-    pub fn new(args: Vec<String>) -> Self {
-        let mut flag = false;
-        let mut targets = Vec::new();
-        for arg in args.iter() {
-            if arg == "-r" || arg == "-R" {
-                flag = true;
-            } else if arg.starts_with('-') {
-                for c in arg[1..].chars() {
-                    if c != 'r' && c != 'R' {
-                        println!("flag: invalid option -- '{}'", c);                       
-                    }
-                }
-
-                flag = true;
-            } else {
-                targets.push(arg.clone());
-            } 
-        }
-        
-        Self {
-            flag,
-            targets,
-        }
+    pub fn new(targets: Vec<String>) -> Self {
+        Self { targets }
     }
 
-    pub fn execute(&self) -> Result<() ,ShellError> {
-        if self.targets.is_empty() {
-           return Err(ShellError::Other("rm: missing operand".into()));
+    pub fn execute(&self) -> Result<(), ShellError> {
+        if self.targets.len() < 2 {
+            return Err(ShellError::Other("cp: missing file operand".into()));
         }
 
-        for target in &self.targets {
-            self.remove_target(target)?;
+        let dest = &self.targets[self.targets.len() - 1];
+            let mut seen = HashSet::new();
+            let mut duplicates = String::new();
+        if self.targets.len() > 2 {
+            if !Path::new(dest).is_dir() {
+                return Err(ShellError::Other(format!(
+                    "cp: target '{}' is not a directory",
+                    dest
+                ).into()));
+            }
+
+            for file in &self.targets[..self.targets.len() - 1] {
+                if Path::new(file).is_file() {
+                    if !seen.insert(file) {
+                        duplicates.push_str(file)
+                    }
+                    let target_path = Path::new(dest).join(Path::new(file).file_name().unwrap());
+                    fs::copy(file, target_path)?;
+                } else {
+                    println!("cp: '{}' is not a file", file);
+                }
+            }
+
+            if !duplicates.is_empty() {
+                 return Err(ShellError::Other(format!("cp: warning: source file '{}' specified more than once", duplicates).into()));
+            }
+            
+        } else {
+            let src = &self.targets[0];
+            if src == dest {  
+                 return Err(ShellError::Other(format!("cp: '{}' and '{}' are the same file", src, dest).into()));
+            }
+            if !Path::new(src).is_file() {
+                return Err(ShellError::Other(format!("cp: '{}' is not a file", src).into()));
+            }
+            fs::copy(src, dest)?;
         }
+
         Ok(())
     }
-
-    pub fn remove_target(&self, target: &str) -> Result<(), ShellError> {
-            if Self::is_protected_path(target) {
-                eprintln!("rm: refusing to remove '.' or '..' : skipping '{}'", target);
-                return Ok(());
-            }
-            let path = Path::new(target);
-            let meta = match fs::symlink_metadata(path) {
-                Ok(m) => m,
-                Err(_) => {
-                    eprintln!("rm: cannot remove '{}': No such file or directory", target);
-                    return Ok(());
-                }
-            };
-
-                if meta.is_symlink() || meta.is_file() {
-                    fs::remove_file(path)
-                        .map_err(|e| ShellError::Other(format!("rm: cannot remove '{}': {}", target, e)))?;
-                        return Ok(());
-                }
-                if meta.is_dir() {
-                    if self.flag {
-                        fs::remove_dir_all(path)
-                            .map_err(|e| ShellError::Other(format!("rm: cannot remove '{}': {}", target, e)))?;
-                    } else {
-                        eprintln!("rm: remove '{}':  Is directory", target);
-                    }
-                }
-                Ok(())
-        }
-
-    pub fn is_protected_path(target: &str) -> bool {
-            matches!(target, "." | "..")
-        }
-
-
-    
 }
